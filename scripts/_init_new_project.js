648e9b6695c7fba12c0e2a247663e34647fe47b3
Fabian <50114802+fabalexsie@users.noreply.github.com>
2023-03-17 17:04:48 +0100

    add init script with self destructing

---
+++ scripts/_init_new_project.js
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require('fs');
+
+// ~https://logfetch.com/js-console-colors/
+const FRMT = {
+  reset: '\x1b[0m',
+  bright: '\x1b[1m',
+  dim: '\x1b[2m',
+  underscore: '\x1b[4m',
+  blink: '\x1b[5m',
+  reverse: '\x1b[7m',
+  hidden: '\x1b[8m',
+  // Foreground (text) colors
+  fg: {
+    black: '\x1b[30m',
+    red: '\x1b[31m',
+    green: '\x1b[32m',
+    yellow: '\x1b[33m',
+    blue: '\x1b[34m',
+    magenta: '\x1b[35m',
+    cyan: '\x1b[36m',
+    white: '\x1b[37m',
+    crimson: '\x1b[38m',
+  },
+  // Background colors
+  bg: {
+    black: '\x1b[40m',
+    red: '\x1b[41m',
+    green: '\x1b[42m',
+    yellow: '\x1b[43m',
+    blue: '\x1b[44m',
+    magenta: '\x1b[45m',
+    cyan: '\x1b[46m',
+    white: '\x1b[47m',
+    crimson: '\x1b[48m',
+  },
+};
+
+// REMOVING this script
+fs.unlink(__filename, () => {
+  console.log(
+    `${FRMT.bg.green}  ${FRMT.reset} ${FRMT.fg.green}Project initialized${FRMT.reset}`
+  );
+});
