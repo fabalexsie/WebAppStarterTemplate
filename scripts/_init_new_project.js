4f12b1f1fd889214d23d6ae7f76b210da16be97c
Fabian <50114802+fabalexsie@users.noreply.github.com>
2023-03-21 16:53:01 +0100

    code restructure (cnsl)

--- scripts/_init_new_project.js
+++ scripts/_init_new_project.js
@@ -2,38 +2,145 @@
 const fs = require('fs');
 const { Stream } = require('stream');
 
-// ~https://logfetch.com/js-console-colors/
-const FRMT = {
-  reset: '\x1b[0m',
-  bright: '\x1b[1m',
-  dim: '\x1b[2m',
-  underscore: '\x1b[4m',
-  blink: '\x1b[5m',
-  reverse: '\x1b[7m',
-  hidden: '\x1b[8m',
-  // Foreground (text) colors
-  fg: {
-    black: '\x1b[30m',
-    red: '\x1b[31m',
-    green: '\x1b[32m',
-    yellow: '\x1b[33m',
-    blue: '\x1b[34m',
-    magenta: '\x1b[35m',
-    cyan: '\x1b[36m',
-    white: '\x1b[37m',
-    crimson: '\x1b[38m',
+const cnsl = {
+  // ~https://logfetch.com/js-console-colors/
+  FRMT: {
+    reset: '\x1b[0m',
+    bright: '\x1b[1m',
+    dim: '\x1b[2m',
+    underscore: '\x1b[4m',
+    blink: '\x1b[5m',
+    reverse: '\x1b[7m',
+    hidden: '\x1b[8m',
+    // Foreground (text) colors
+    fg: {
+      black: '\x1b[30m',
+      red: '\x1b[31m',
+      green: '\x1b[32m',
+      yellow: '\x1b[33m',
+      blue: '\x1b[34m',
+      magenta: '\x1b[35m',
+      cyan: '\x1b[36m',
+      white: '\x1b[37m',
+      crimson: '\x1b[38m',
+    },
+    // Background colors
+    bg: {
+      black: '\x1b[40m',
+      red: '\x1b[41m',
+      green: '\x1b[42m',
+      yellow: '\x1b[43m',
+      blue: '\x1b[44m',
+      magenta: '\x1b[45m',
+      cyan: '\x1b[46m',
+      white: '\x1b[47m',
+      crimson: '\x1b[48m',
+    },
   },
-  // Background colors
-  bg: {
-    black: '\x1b[40m',
-    red: '\x1b[41m',
-    green: '\x1b[42m',
-    yellow: '\x1b[43m',
-    blue: '\x1b[44m',
-    magenta: '\x1b[45m',
-    cyan: '\x1b[46m',
-    white: '\x1b[47m',
-    crimson: '\x1b[48m',
+  wip: (...args) =>
+    args.forEach((arg) =>
+      console.log(`${cnsl.FRMT.fg.cyan}\u25A0 ${arg}${cnsl.FRMT.reset}`)
+    ),
+  success: (...args) =>
+    args.forEach((arg) =>
+      console.log(`${cnsl.FRMT.fg.green}\u25A0 ${arg}${cnsl.FRMT.reset}`)
+    ),
+  error: (...args) =>
+    args.forEach((arg) =>
+      console.log(`${cnsl.FRMT.fg.red}\u25A0 ${arg}${cnsl.FRMT.reset}`)
+    ),
+  info: (...args) =>
+    args.forEach((arg) =>
+      console.log(`${cnsl.FRMT.fg.blue}\u25A0 ${arg}${cnsl.FRMT.reset}`)
+    ),
+  answer: (...args) =>
+    args.forEach((arg) =>
+      console.log(`${cnsl.FRMT.fg.yellow}${arg}${cnsl.FRMT.reset}`)
+    ),
+  textInputQuestion: (question) => {
+    return new Promise((resolve) => {
+      console.log(question);
+
+      process.stdout.write(cnsl.FRMT.fg.yellow);
+      process.stdin.resume();
+      process.stdin.on('data', (data) => {
+        process.stdout.write(cnsl.FRMT.reset);
+        process.stdin.pause();
+        console.log();
+        resolve(data.trim()); // remove new line
+      });
+    });
+  },
+  multipleChoiceQuestion: async (questObj) => {
+    // ~https://stackoverflow.com/a/30687420
+    return new Promise((resolve) => {
+      let stdin = process.stdin;
+      let stdout = process.stdout;
+      stdin.setRawMode(true);
+      stdin.resume();
+      stdin.setEncoding('utf8');
+      stdout.write('\u001b[?25l'); // hide cursor
+
+      const answers = questObj.answers;
+      let selectedOption = questObj.defaultAnswer || 0;
+
+      const printAnswers = async (selectedOption) => {
+        await promOut.moveCursor(0);
+        await promOut.clearScreenDown();
+
+        for (const i in answers) {
+          const answ = answers[i];
+          if (i == selectedOption) {
+            await promOut.write(
+              `${cnsl.FRMT.fg.yellow}> ${answ}${cnsl.FRMT.reset}\n`
+            );
+          } else {
+            await promOut.write(
+              `${cnsl.FRMT.fg.blue}${answ}${cnsl.FRMT.reset}\n`
+            );
+          }
+        }
+
+        await promOut.moveCursor(0, -answers.length);
+      };
+
+      console.log(`${questObj.quest}`);
+      printAnswers(selectedOption);
+
+      const keyListener = async (key) => {
+        if (key == '\u001B\u005B\u0041' || key == '\u001B\u005B\u0044') {
+          // up or left
+          selectedOption--;
+          selectedOption =
+            selectedOption < 0 ? answers.length - 1 : selectedOption;
+          await printAnswers(selectedOption);
+        } else if (key == '\u001B\u005B\u0043' || key == '\u001B\u005B\u0042') {
+          // right or down
+          selectedOption++;
+          selectedOption %= answers.length;
+          await printAnswers(selectedOption);
+        } else if (key == '\u000D' || key == '\u0020') {
+          // enter or space
+          await promOut.moveCursor(0);
+          await stdout.clearScreenDown();
+          stdin.removeListener('data', keyListener);
+          stdin.setRawMode(false);
+          stdin.pause();
+          stdout.write('\u001b[?25h'); // show cursor
+          cnsl.answer(answers[selectedOption]);
+          console.log();
+          resolve(selectedOption);
+        } else if (key == '\u0003') {
+          // ctrl-c
+          await promOut.moveCursor(0);
+          await stdout.clearScreenDown();
+          stdin.removeListener('data', keyListener);
+          process.exit();
+        }
+      };
+
+      stdin.addListener('data', keyListener);
+    });
   },
 };
 
@@ -64,27 +171,6 @@ const TXT = {
   },
 };
 
-const log = {
-  wip: (...args) =>
-    args.forEach((arg) =>
-      console.log(`${FRMT.fg.cyan}\u25A0 ${arg}${FRMT.reset}`)
-    ),
-  success: (...args) =>
-    args.forEach((arg) =>
-      console.log(`${FRMT.fg.green}\u25A0 ${arg}${FRMT.reset}`)
-    ),
-  error: (...args) =>
-    args.forEach((arg) =>
-      console.log(`${FRMT.fg.red}\u25A0 ${arg}${FRMT.reset}`)
-    ),
-  info: (...args) =>
-    args.forEach((arg) =>
-      console.log(`${FRMT.fg.blue}\u25A0 ${arg}${FRMT.reset}`)
-    ),
-  answer: (...args) =>
-    args.forEach((arg) => console.log(`${FRMT.fg.yellow}${arg}${FRMT.reset}`)),
-};
-
 // make stdout promisable
 const promOut = {
   moveCursor: (absX, relY) => {
@@ -112,92 +198,9 @@ const promOut = {
   },
 };
 
-// ~https://stackoverflow.com/a/30687420
-async function multipleChoiceQuestion(questObj) {
-  return new Promise((resolve) => {
-    let stdin = process.stdin;
-    let stdout = process.stdout;
-    stdin.setRawMode(true);
-    stdin.resume();
-    stdin.setEncoding('utf8');
-    stdout.write('\u001b[?25l'); // hide cursor
-
-    const answers = questObj.answers;
-    let selectedOption = questObj.defaultAnswer || 0;
-
-    const printAnswers = async (selectedOption) => {
-      await promOut.moveCursor(0);
-      await promOut.clearScreenDown();
-
-      for (const i in answers) {
-        const answ = answers[i];
-        if (i == selectedOption) {
-          await promOut.write(`${FRMT.fg.yellow}> ${answ}${FRMT.reset}\n`);
-        } else {
-          await promOut.write(`${FRMT.fg.blue}${answ}${FRMT.reset}\n`);
-        }
-      }
-
-      await promOut.moveCursor(0, -answers.length);
-    };
-
-    console.log(`${questObj.quest}`);
-    printAnswers(selectedOption);
-
-    const keyListener = async (key) => {
-      if (key == '\u001B\u005B\u0041' || key == '\u001B\u005B\u0044') {
-        // up or left
-        selectedOption--;
-        selectedOption =
-          selectedOption < 0 ? answers.length - 1 : selectedOption;
-        await printAnswers(selectedOption);
-      } else if (key == '\u001B\u005B\u0043' || key == '\u001B\u005B\u0042') {
-        // right or down
-        selectedOption++;
-        selectedOption %= answers.length;
-        await printAnswers(selectedOption);
-      } else if (key == '\u000D' || key == '\u0020') {
-        // enter or space
-        await promOut.moveCursor(0);
-        await stdout.clearScreenDown();
-        stdin.removeListener('data', keyListener);
-        stdin.setRawMode(false);
-        stdin.pause();
-        stdout.write('\u001b[?25h'); // show cursor
-        log.answer(answers[selectedOption]);
-        console.log();
-        resolve(selectedOption);
-      } else if (key == '\u0003') {
-        // ctrl-c
-        await promOut.moveCursor(0);
-        await stdout.clearScreenDown();
-        stdin.removeListener('data', keyListener);
-        process.exit();
-      }
-    };
-
-    stdin.addListener('data', keyListener);
-  });
-}
-
-function textInputQuestion(question) {
-  return new Promise((resolve) => {
-    console.log(question);
-
-    process.stdout.write(FRMT.fg.yellow);
-    process.stdin.resume();
-    process.stdin.on('data', (data) => {
-      process.stdout.write(FRMT.reset);
-      process.stdin.pause();
-      console.log();
-      resolve(data.trim()); // remove new line
-    });
-  });
-}
-
-async function startProcess(res, txtKeyStart, txtKeyFinished, workerFnc) {
+async function startSubProcess(res, txtKeyStart, txtKeyFinished, workerFnc) {
   return new Promise((resolve, reject) => {
-    log.wip(TXT[txtKeyStart][res.language]);
+    cnsl.wip(TXT[txtKeyStart][res.language]);
 
     const outStream = new Stream.Writable();
     let lines = ['', '', ''];
@@ -224,13 +227,13 @@ async function startProcess(res, txtKeyStart, txtKeyFinished, workerFnc) {
         .then(async () => {
           await promOut.moveCursor(0, -4);
           await promOut.clearScreenDown();
-          log.success(TXT[txtKeyFinished][res.language]);
+          cnsl.success(TXT[txtKeyFinished][res.language]);
           resolve(res);
         })
         .catch(async () => {
           await promOut.moveCursor(0, -4);
           await promOut.clearScreenDown();
-          log.error(`${TXT[txtKeyStart][res.language]} (ERROR)`);
+          cnsl.error(`${TXT[txtKeyStart][res.language]} (ERROR)`);
           reject();
         });
     });
@@ -238,7 +241,7 @@ async function startProcess(res, txtKeyStart, txtKeyFinished, workerFnc) {
 }
 
 async function getConfig() {
-  const langNo = await multipleChoiceQuestion({
+  const langNo = await cnsl.multipleChoiceQuestion({
     quest: 'Language?',
     answers: ['deutsch', 'english'],
     defaultAnswer: 1,
@@ -248,15 +251,17 @@ async function getConfig() {
 
   return {
     language: langConv[langNo],
-    projectName: await textInputQuestion(TXT['proj_name'][langConv[langNo]]),
+    projectName: await cnsl.textInputQuestion(
+      TXT['proj_name'][langConv[langNo]]
+    ),
   };
 }
 
-process.stdout.write(FRMT.reset);
+process.stdout.write(cnsl.FRMT.reset);
 getConfig()
   .then((res) =>
-    // RENAME app to the given name
-    startProcess(
+    // RENAME app to the given name // TODO
+    startSubProcess(
       res,
       'renaming_start',
       'renaming_finished',
@@ -266,13 +271,37 @@ getConfig()
           setTimeout(() => myOut.write('Doing step 3'), 500);
           setTimeout(() => myOut.write('step 4'), 1000);
           setTimeout(() => myOut.write('5 finished'), 1500);
-          setTimeout(resolve, 2_000); // TODO
+          setTimeout(resolve, 2_000);
         })
     )
   )
   .then((res) =>
-    // REMOVE *.env from git
-    startProcess(
+    // REMOVE *.env from git // TODO
+    startSubProcess(
+      res,
+      'git_env_start',
+      'git_env_finished',
+      (myOut) =>
+        new Promise((resolve, reject) => {
+          myOut.write('Doinjg step 1\ndoing step 2', () => resolve());
+        })
+    )
+  )
+  .then((res) =>
+    // INSTALL npm in frontend // TODO
+    startSubProcess(
+      res,
+      'git_env_start',
+      'git_env_finished',
+      (myOut) =>
+        new Promise((resolve, reject) => {
+          myOut.write('Doinjg step 1\ndoing step 2', () => resolve());
+        })
+    )
+  )
+  .then((res) =>
+    // INSTALL npm in server // TODO
+    startSubProcess(
       res,
       'git_env_start',
       'git_env_finished',
@@ -285,15 +314,15 @@ getConfig()
   .then((res) => {
     // REMOVING this script
     fs.unlink(__filename, () => {
-      log.success('Removed this script');
+      cnsl.success('Removed this script');
 
-      // DO a commit
-      log.info(
+      // ASK for doing a commit // TODO committen (..text..)?: <Oder was anderes hier eingeben>
+      cnsl.info(
         'Please do a commit to save this initial state. For example with:'
       );
       console.log(`git commit -m "Initialized project '${res.projectName}'"`);
 
-      log.success(TXT['proj_inited'][res.language]);
+      cnsl.success(TXT['proj_inited'][res.language]);
     });
   })
   .catch((err) => {
@@ -302,10 +331,10 @@ getConfig()
   });
 
 process.on('SIGINT', () => {
-  process.stdout.write(FRMT.reset);
+  process.stdout.write(cnsl.FRMT.reset);
   process.exit(0);
 });
 process.on('SIGTERM', () => {
-  process.stdout.write(FRMT.reset);
+  process.stdout.write(cnsl.FRMT.reset);
   process.exit(0);
 });
